bfc625c8299e120ef16e4161f2c7c5dd20122c74
Himanshu <himanshu16315@gmail.com>
2023-03-03 02:17:12 -0500

    Initialized project and added splash screen

---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'screens/splash/splash_screen.dart';
+
+
+// We use name route
+// All our routes will be available here
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+};
---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:milestone/size_config.dart';
+import 'package:milestone/constants.dart';
+
+import 'splash_content.dart';
+import 'package:milestone/components/default_btn.dart';
+
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "We care for you, as you wanted",
+      "image": "assets/images/splash.jpg"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]["image"],
+                  text: splashData[index]['text'],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(flex: 2),
+                    DefaultBtn(
+                      text: "Continue",
+                      press: () {
+                        // Navigator.pushNamed(context, SignInScreen.routeName);
+                      },
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({int? index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
